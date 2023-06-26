68035cec5d4aa6a9b91f12a278d41b9dca80d4e1
yego5 <shachz.github@gmail.com>
2023-06-25 18:05:09 -0700

    add.c file

--- add.c
+++ add.c
@@ -1,23 +1,22 @@
 #include "monty.h"
-
 /**
- * f_add - adds the top two elements of the stack.
- * @head: stack head
+ * f_add - adds top two elements of the stack.
+ * @head: the stack head
  * @counter: line_number
  * Return: no return
 */
 void f_add(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
-	int ln = 0, aux;
+	int len = 0, aux;
 
 	h = *head;
 	while (h)
 	{
 		h = h->next;
-		ln++;
+		len++;
 	}
-	if (ln < 2)
+	if (len < 2)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
 		fclose(bus.file);
